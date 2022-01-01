2f5716b47f1173915f2041a843c271ee12950899
Shane-Gray394 <86175936+Shane-Gray394@users.noreply.github.com>
2021-12-31 20:13:39 -0500

    wip

--- util/mongodb.js
+++ util/mongodb.js
@@ -1,14 +1,12 @@
 import { MongoClient } from 'mongodb';
 
-const { MONGODB_URI, MONGODB_DB } = process.env;
-
-if (!MONGODB_URI) {
+if (!process.env.MONGODB_URI) {
   throw new Error(
     'Please define the MONGODB_URI environment variable inside .env.local'
   );
 }
 
-if (!MONGODB_DB) {
+if (!process.env.MONGODB_DB) {
   throw new Error(
     'Please define the MONGODB_DB environment variable inside .env.local'
   );
@@ -31,12 +29,14 @@ export async function connectToDatabase() {
       useUnifiedTopology: true,
     };
 
-    cached.promise = MongoClient.connect(MONGODB_URI, opts).then((client) => {
-      return {
-        client,
-        db: client.db(MONGODB_DB),
-      };
-    });
+    cached.promise = MongoClient.connect(process.env.MONGODB_URI, opts).then(
+      (client) => {
+        return {
+          client,
+          db: client.db(process.env.MONGODB_DB),
+        };
+      }
+    );
   }
   cached.conn = await cached.promise;
   return cached.conn;
